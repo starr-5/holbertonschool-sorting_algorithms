7c275c85e2c01b75703a638507cf155acdf7db2b
starr-5 <studying12567@gmail.com>
2026-02-06 07:14:54 +0000

    Write a function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -16,5 +16,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Array sorting */
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
+
+/* Helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
