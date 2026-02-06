c2ab7d3a25854abfedd63d1d80a796b4f7c4d860
starr-5 <studying12567@gmail.com>
2026-02-06 08:19:05 +0000

    Write a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- sort.h
+++ sort.h
@@ -1,10 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
-/* Forward declaration of doubly linked list node */
+/* Doubly linked list node structure for insertion_sort_list */
 typedef struct listint_s
 {
     int n;
@@ -12,7 +11,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
